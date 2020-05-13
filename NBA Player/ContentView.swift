b377a85e1097b94c461160045750543f710c1a98
Julio Collado <jcollado@altimetrik.com>
2020-05-13 10:21:01 -0300

    NBAPlayer: Base Project

--- NBA Player/ContentView.swift
+++ NBA Player/ContentView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Image("gs").resizable().aspectRatio(contentMode: .fit)
+            Image("steph").clipShape(Circle()).background(Circle().foregroundColor(.white)).overlay(Circle().stroke(Color.white, lineWidth: 4)).offset(x: 0, y: -90).padding(.bottom, -90).shadow(radius: 15)
+            Text("Stephen Curry").font(.system(size: 45)).bold()
+            StatText(statName: "Age", statValue: "31")
+            StatText(statName: "Height", statValue: "6' 3\"")
+            StatText(statName: "Wright", statValue: "190lbs")
+            Spacer()
+        }.edgesIgnoringSafeArea(.top)
     }
 }
 
---
+++ NBA Player/StatText.swift
@@ -0,0 +1,34 @@
+//
+//  StatText.swift
+//  NBA Player
+//
+//  Created by Julio Collado on 5/13/20.
+//  Copyright © 2020 julio. All rights reserved.
+//
+
+import SwiftUI
+
+struct StatText: View {
+    
+    var statName: String
+    var statValue: String
+    
+//    init(statName: String, statValue: String) {
+//        self.statName = statName
+//        self.statValue = statValue
+//    }
+    
+    var body: some View {
+        HStack {
+            Text(statName + ":").padding(.leading, 30).font(Font.system(size: 45).bold())
+            Text(statValue).font(.system(size: 45)).fontWeight(.light).padding(.trailing, 30)
+            Spacer()
+        }
+    }
+}
+
+struct StatText_Previews: PreviewProvider {
+    static var previews: some View {
+        StatText(statName: "Age", statValue: "31")
+    }
+}
