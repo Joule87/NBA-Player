66a91e918d74560cb8a27cf9ef454116bf1ce4df
Julio Collado <jcollado@altimetrik.com>
2020-05-13 09:21:26 -0300

    Initial Commit

---
+++ NBA Player/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NBA Player
+//
+//  Created by Julio Collado on 5/13/20.
+//  Copyright © 2020 julio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NBA PlayerTests/NBA_PlayerTests.swift
@@ -0,0 +1,34 @@
+//
+//  NBA_PlayerTests.swift
+//  NBA PlayerTests
+//
+//  Created by Julio Collado on 5/13/20.
+//  Copyright © 2020 julio. All rights reserved.
+//
+
+import XCTest
+@testable import NBA_Player
+
+class NBA_PlayerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
